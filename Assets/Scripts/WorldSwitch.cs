7410660c2bac6b0904d486ec1b87fd3f43c0fe4e
Bimbles <64385517+Vintovin@users.noreply.github.com>
2022-03-05 12:40:43 +0000

    TimeShift Test
    Testing

---
+++ Assets/Scripts/WorldSwitch.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]public GameObject World1;
+    [SerializeField]public GameObject World2;
+    private bool SecondWorld = false;
+
+
+    void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            if (SecondWorld == false) 
+            {
+                World1.SetActive(false);
+                World2.SetActive(true);
+                SecondWorld = true;
+            }
+            else
+            {
+                World1.SetActive(true);
+                World2.SetActive(false);
+                SecondWorld = false;
+            }
+        }
+    }
+}
