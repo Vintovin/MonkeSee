e4efd6c5cf5efc25ff40546ec5b3e656c45fce3f
Bimbles <64385517+Vintovin@users.noreply.github.com>
2022-05-07 11:48:24 +0100

    Coin Update
    Mana and Banana Coins Added to prefabs

---
+++ Assets/Scripts/CoinHandling.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinHandling : MonoBehaviour
+{
+    [SerializeField] private GameObject Coin;
+    [SerializeField] private GameObject plr;
+    [SerializeField] private Sprite[] Slist;
+    [SerializeField] private WorldSwitch WSHandle;
+    [SerializeField] private PlayerHealth PHHandle;
+
+    ////////////////////////////////////////////
+    public enum CoinTypes {Health,Mana};
+    public CoinTypes CT;
+    public int HealthIncriment;
+    public int ManaIncriment;
+    ////////////////////////////////////////////
+    ////////////////////////////////////////////
+    private SpriteRenderer SR;
+    ////////////////////////////////////////////
+    ////////////////////////////////////////////
+    private int CoinWait = 15;
+    private float CF = 0;
+    private int SpriteCount = 0;
+    ////////////////////////////////////////////
+    ////////////////////////////////////////////
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+    ///////////////////////////////////////////
+    void ChangeSprite()
+    {
+        SpriteCount = SpriteCount+ 1;
+        if(SpriteCount == Slist.Length){
+            SpriteCount = 0;
+        }
+        SR.sprite = Slist[SpriteCount]; 
+    }
+    ///////////////////////////////////////////
+    void Add()
+    {
+        if(CT == CoinTypes.Health)
+        {
+            if(PHHandle.currentHealth != PHHandle.maxHealth)
+            {
+                float NewHealth = PHHandle.currentHealth+HealthIncriment;
+                if(NewHealth > PHHandle.maxHealth)
+                {
+                    PHHandle.currentHealth = PHHandle.maxHealth;
+                }else{
+                    PHHandle.currentHealth = NewHealth;
+                }
+                Coin.SetActive(false);
+            }
+        }else if(CT == CoinTypes.Mana)
+        {
+            if(WSHandle.Cur_Charge != WSHandle.Def_Charge)
+            {
+                float NewMana = WSHandle.Cur_Charge + ManaIncriment;
+                if(NewMana > WSHandle.Def_Charge)
+                {
+                    WSHandle.Cur_Charge = WSHandle.Def_Charge;
+                }else
+                {
+                    WSHandle.Cur_Charge = NewMana;
+                    WSHandle.UpdateUI();
+                    
+                }
+                Coin.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D col) {
+        var hitObject = col.gameObject;
+        if(col.gameObject == plr)
+        {
+            Add();
+        }
+        
+    }
+
+
+    ///////////////////////////////////////////
+    void Update()
+    {
+        CF = CF+1;
+        if(CF == CoinWait){
+            CF = 0;
+            ChangeSprite();
+            
+        }
+
+        
+        
+    }
+}
